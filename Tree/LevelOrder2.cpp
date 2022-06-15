9593df2d71ba39b5a4bdcdde126b36db2fbb22b3
harit1412 <202103039@daiict.ac.in>
2022-06-15 17:36:53 +0530

    level2

---
+++ Tree/LevelOrder2.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+void LevelOrder(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (q.size() > 1)
+    {
+        Node *curr = q.front();
+        q.pop();
+
+        if (curr == NULL)
+        {
+            cout << "\n";
+            q.push(NULL);
+            continue;
+        }
+        cout << curr->key << " ";
+        if (curr->left != NULL)
+            q.push(curr->left);
+        if (curr->right != NULL)
+            q.push(curr->right);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->right->left = new Node(40);
+    root->right->right = new Node(50);
+    LevelOrder(root);
+    return 0;
+}
