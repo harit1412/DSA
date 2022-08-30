252e1c24bad10ba414e0cbbeebf182ac4bfae11b
Harit Rajkotiya <202103039@daiict.ac.in>
2022-08-30 17:38:12 +0530

    s

--- Stack/Next_Greater.cpp
+++ Stack/Next_Greater.cpp
@@ -2,28 +2,33 @@
 using namespace std;
 // #important
 
-
-//It will Print in reverse we can store it in an vector and print in reverse order {given in gfg}
-
+// It will Print in reverse we can store it in an vector and print in reverse order {given in gfg}
 
 void PrintGreater(int a[])
 {
     stack<int> s;
     s.push(7);
-    cout << "-1 ";
+    // cout << "-1 ";
+    vector<int> v;
+    v.push_back(-1);
     for (int i = 6; i >= 0; i--)
     {
-        while(!s.empty() && a[i]>=a[s.top()])
+        while (!s.empty() && a[i] >= a[s.top()])
         {
             s.pop();
         }
-        if(s.empty())
+        if (s.empty())
             cout << "-1 ";
         else
         {
-            cout << a[s.top()] << " ";
+            v.push_back(a[s.top()]);
+            // cout << a[s.top()] << " ";
         }
         s.push(i);
+    }
+    for (auto j : v)
+    {
+        cout << j << " ";
     }
 }
 int main(int argc, char const *argv[])
